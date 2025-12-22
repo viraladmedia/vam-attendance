77c8d1cd8a96b7db34574a8ef08021d24f17b76f
AzeezBello <demolablaze@gmail.com>
2025-12-22 09:46:45 -0600

    fix

--- app/dashboard/attendance/page.tsx
+++ app/dashboard/attendance/page.tsx
@@ -1,6 +1,8 @@
-"use client";
-
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
-export { default } from "./page.client";
+import AttendancePageClient from "./page.client";
+
+export default function AttendancePage() {
+  return <AttendancePageClient />;
+}
