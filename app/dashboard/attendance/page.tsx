0964f9c4197e802ca2f979f879df101b7c8cd031
AzeezBello <demolablaze@gmail.com>
2025-12-22 09:05:43 -0600

    fixed

--- app/dashboard/attendance/page.tsx
+++ app/dashboard/attendance/page.tsx
@@ -1,13 +1,6 @@
+"use client";
+
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
-import NextDynamic from "next/dynamic";
-
-const AttendanceClient = NextDynamic(() => import("./page.client"), {
-  ssr: false,
-  loading: () => null,
-});
-
-export default function AttendancePage() {
-  return <AttendanceClient />;
-}
+export { default } from "./page.client";
